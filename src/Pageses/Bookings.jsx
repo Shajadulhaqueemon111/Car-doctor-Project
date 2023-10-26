107cc048a7df92eb0b3908c25624e159c40285e6
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-10-26 20:36:41 +0600

    index.html

--- src/Pageses/Bookings.jsx
+++ src/Pageses/Bookings.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../Components/AuthProvider/AuthProvider';
 import BooksCard from './BooksCard';
+import Swal from 'sweetalert2';
 
 const Bookings = () => {
     const { user } = useContext(AuthContext)
@@ -15,6 +16,78 @@ const Bookings = () => {
 
             )
     }, [])
+    const handelDelete=(id)=>{
+      Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+              fetch(`http://localhost:5000/bookings/${id}`,{
+                  method:"DELETE"
+              })
+              .then(res=>res.json())
+              .then(data=>{
+                  console.log(data)
+                  if(data. deletedCount>0){
+                      Swal.fire(
+                          'Deleted!',
+                          'Your file has been deleted.',
+                          'success'
+                        )
+                        const remaining=bookings.filter(booking=>booking._id !== id)
+                        setBookings(remaining)
+
+                  }
+              })
+          
+          }
+        })
+  }
+    const handelConfirm=(id)=>{
+      Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, Confirm it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+              fetch(`http://localhost:5000/bookings/${id}`,{
+                  method:"PATCH",
+                  headers:{
+                    "Content-Type":"application/json"
+
+                  },
+                  body:JSON.stringify({status:"confirm"})
+              })
+              .then(res=>res.json())
+              .then(data=>{
+                  console.log(data)
+                  if(data.  modifiedCount){
+                      Swal.fire(
+                          'Confirm!',
+                          'Your Booking updated has been successfully.',
+                          'success'
+                        )
+                        const remaining=bookings.filter(booking=>booking._id !== id)
+                        const updated=bookings.find(booking=>booking._id===id)
+                        updated.status='confirm'
+                        const newBooking=[updated,...remaining]
+                        setBookings(newBooking)
+
+                  }
+              })
+          
+          }
+        })
+  }
     return (
         <div>
             <h2>My Bookings:{bookings.length}</h2>
@@ -27,14 +100,19 @@ const Bookings = () => {
             <input type="checkbox" className="checkbox" />
           </label>
         </th>
+        <th className="py-2 text-blue-500">Image</th>
         <th className="py-2 text-blue-500">Name</th>
-        <th className="py-2 text-green-500">Job</th>
-        <th className="py-2">Favorite Color</th>
-        <th className="py-2"></th>
+        <th className="py-2 text-green-500">service</th>
+        <th className="py-2 text-green-500">DAte</th>
+        <th className="py-2">Gmail</th>
+        <th className="py-2">Price</th>
       </tr>
     </thead>
     <tbody>
-      {bookings?.map((booking) =><BooksCard key={booking._id} booking={booking}></BooksCard>
+      {bookings?.map((booking) =><BooksCard key={booking._id}
+      handelDelete={handelDelete}
+      handelConfirm={handelConfirm}
+      booking={booking}></BooksCard>
        
       )}
     </tbody>
--- src/Pageses/BooksCard.jsx
+++ src/Pageses/BooksCard.jsx
@@ -1,39 +1,51 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
-const BooksCard = ({booking}) => {
+const BooksCard = ({ booking,handelDelete,handelConfirm,status }) => {
 
-    const {customerName,email,img,service,date,price}=booking
+    const {_id, customerName, email, img, service, date, price } = booking
+
+  
+  
     return (
         <div>
-              <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className=" w-24 rounded h-24">
-                                            <img src={img} alt="Avatar Tailwind CSS Component" />
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                              {service}
-                            </td>
-                            <td>{email}</td>
-                            <td>${price}</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>
-                      
+            <tr>
+                <th>
+                    <button onClick={()=>handelDelete(_id)} className="btn btn-square btn-outline">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+                </th>
+                <td>
+                    <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                            <div className=" w-24 rounded h-24">
+                                <img src={img} alt="Avatar Tailwind CSS Component" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">Hart Hagerty</div>
+                            <div className="text-sm opacity-50">United States</div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    {customerName}
+                </td>
+                <td>
+                    {service}
+                </td>
+                <td>
+                    {date}
+                </td>
+                <td>{email}</td>
+                <td>${price}</td>
+                <th>
+                {
+                    status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> :
+                        <button onClick={() => handelConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirm</button>}
+                </th>
+            </tr>
+
         </div>
     );
 };
